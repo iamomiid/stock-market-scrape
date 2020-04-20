abedb525e371df73d6c88171e5d98225d067a558
Omid Sayfun <seyfanomid@ymail.com>
2020-04-21 01:26:04 +0430

    Formatting Handled

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Puppeteer from 'puppeteer';
+import moment from 'moment-jalaali';
 import cheerio from 'cheerio';
 import { symbols } from './config';
 
@@ -16,59 +17,86 @@ enum Keys {
 	Value,
 	Volume,
 	Count,
-	Date,
+	DateJalaali,
+	PreviousDate,
 }
 
-type key = keyof typeof Keys;
+type RowKey = keyof typeof Keys;
+
+type DateObject = { [key in RowKey]?: Number | string };
 
 function getEnumKeyByEnumValue(enumValue: number) {
-	let keys = Object.keys(Keys).filter((x) => Keys[x as key] == enumValue) as key[];
+	let keys = Object.keys(Keys).filter((x) => Keys[x as RowKey] == enumValue) as RowKey[];
 	return keys[0]!;
 }
 
-const key = getEnumKeyByEnumValue(0);
-console.log(key)
+function toNumber(input: string) {
+	return Number(input.trim().replace(/,/g, ''));
+}
 
-// class StockScrape {
-// 	browser!: Puppeteer.Browser;
-// 	async scrape(id: number, pageNumber = 1) {
-// 		const page = await this.browser.newPage();
-// 		await page.goto(`http://tsetmc.com/Loader.aspx?ParTree=151311&i=${id}`, { waitUntil: 'domcontentloaded' });
-// 		const selector = 'a.torquoise';
-// 		await page.evaluate((selector) => document.querySelectorAll(selector)[0].click(), selector);
-// 		await new Promise((r) => setTimeout(r, 3000));
-// 		const $ = cheerio.load(await page.content());
-// 		$('.objbox .obj tr').each((_, i) => {
-// 			const arr: CheerioElement[] = [];
-// 			i.children
-// 				.filter((v) => v.name == 'td')
-// 				.forEach((v) =>
-// 					v.children.forEach((vi) => {
-// 						if (vi.type == 'text') {
-// 							arr.push(vi);
-// 						} else if (vi.name == 'span') {
-// 							arr.push(...vi.children);
-// 						}
-// 					})
-// 				);
-// 			for (const element of arr) {
-// 				console.log(element.data);
-// 			}
-// 		});
-// 		const selector2 = '.pagingBlock a';
-// 		await page.evaluate((selector) => document.querySelectorAll(selector)[1].click(), selector2);
-// 		await page.screenshot({ path: 'example.png' });
-// 	}
+function toDate(input: string) {
+	return input;
+}
 
-// 	async start() {
-// 		this.browser = await Puppeteer.launch({ defaultViewport: { width: 1920, height: 2000 } });
-// 		for await (const symbol of symbols) {
-// 			await this.scrape(symbol.id);
-// 			await new Promise((r) => setTimeout(r, 5000));
-// 		}
-// 		await this.browser.close();
-// 	}
-// }
+class StockScrape {
+	browser!: Puppeteer.Browser;
 
-// const scrapper = new StockScrape();
-// scrapper.start();
+	async scrape(id: number, pageNumber = 1) {
+		const page = await this.browser.newPage();
+		await page.goto(`http://tsetmc.com/Loader.aspx?ParTree=151311&i=${id}`, {
+			waitUntil: 'domcontentloaded',
+		});
+		const selector = 'a.torquoise';
+		await page.evaluate((selector) => document.querySelectorAll(selector)[0].click(), selector);
+		await new Promise((r) => setTimeout(r, 3000));
+		const $ = cheerio.load(await page.content());
+		const arr: CheerioElement[] = [];
+		$('.objbox .obj tr').each((_, i) => {
+			i.children
+				.filter((v) => v.name == 'td')
+				.forEach((v) =>
+					v.children.forEach((vi) => {
+						if (vi.type == 'text') {
+							arr.push(vi);
+						} else if (vi.name == 'span') {
+							arr.push(...vi.children);
+						}
+					})
+				);
+		});
+		const total: DateObject[] = [];
+		let obj: DateObject = {};
+		let i = 0;
+		for (const element of arr) {
+			if (element.data!.trim().length == 0) {
+				console.log(obj);
+				total.push(obj);
+				i = 0;
+				obj = {};
+			} else {
+				const enumKey = getEnumKeyByEnumValue(i++);
+				if (enumKey == 'PreviousDate') continue;
+				if (enumKey == 'DateJalaali') {
+					obj[enumKey] = toDate(element.data!);
+				} else {
+					obj[enumKey] = toNumber(element.data!);
+				}
+			}
+		}
+		const selector2 = '.pagingBlock a';
+		await page.evaluate((selector) => document.querySelectorAll(selector)[1].click(), selector2);
+		await page.screenshot({ path: 'example.png' });
+	}
+
+	async start() {
+		this.browser = await Puppeteer.launch({ defaultViewport: { width: 1920, height: 1080 } });
+		for await (const symbol of symbols) {
+			await this.scrape(symbol.id);
+			await new Promise((r) => setTimeout(r, 5000));
+		}
+		await this.browser.close();
+	}
+}
+
+const scrapper = new StockScrape();
+scrapper.start();
