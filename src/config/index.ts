43a76beaebb1352bcbde6eb3fedfb90f9b554366
Omid Sayfun <seyfanomid@ymail.com>
2020-04-21 16:25:05 +0430

    Changed BigInt to string

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,13 @@
 export interface Symbol {
-	id: BigInt;
+	id: string;
 	code: string;
 	days: number;
 }
 
 export const symbols: Symbol[] = [
-	{ id: BigInt('20562694899904339'), code: 'PRDZ1', days: 200 },
-	{ id: BigInt('7711282667602555'), code: 'PARK1', days: 200 },
-	{ id: BigInt('46348559193224090'), code: 'FOLD1', days: 200 },
-	{ id: BigInt('44013656953678055'), code: 'NOLZ1', days: 200 },
-	{ id: BigInt('1625149423498289'), code: 'SMBZ1', days: 200 },
+	{ id: '20562694899904339', code: 'PRDZ1', days: 200 },
+	{ id: '7711282667602555', code: 'PARK1', days: 200 },
+	{ id: '46348559193224090', code: 'FOLD1', days: 200 },
+	{ id: '44013656953678055', code: 'NOLZ1', days: 200 },
+	{ id: '1625149423498289', code: 'SMBZ1', days: 200 },
 ];
