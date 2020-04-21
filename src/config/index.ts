330d08c848703aef626ab93b96690fcc7454d6dd
Omid Sayfun <seyfanomid@ymail.com>
2020-04-21 16:12:29 +0430

    Formated and Added 5 Symbols

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,13 @@
-interface Stock {
-	id: number;
-	symbol: string;
+export interface Symbol {
+	id: BigInt;
+	code: string;
+	days: number;
 }
 
-export const symbols: Stock[] = [{ id: 778253364357513, symbol: 'Wabelmat' }];
+export const symbols: Symbol[] = [
+	{ id: BigInt('20562694899904339'), code: 'PRDZ1', days: 200 },
+	{ id: BigInt('7711282667602555'), code: 'PARK1', days: 200 },
+	{ id: BigInt('46348559193224090'), code: 'FOLD1', days: 200 },
+	{ id: BigInt('44013656953678055'), code: 'NOLZ1', days: 200 },
+	{ id: BigInt('1625149423498289'), code: 'SMBZ1', days: 200 },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import Puppeteer from 'puppeteer';
 import moment from 'moment-jalaali';
+import fs from 'fs';
+import path from 'path';
+import * as csv from 'fast-csv';
 import cheerio from 'cheerio';
-import { symbols } from './config';
+import { symbols, Symbol } from './config';
+
+moment.loadPersian();
 
 enum Keys {
 	HighestPrice,
@@ -19,6 +24,8 @@ enum Keys {
 	Count,
 	DateJalaali,
 	PreviousDate,
+	DayOfWeek,
+	WeekOfYear,
 }
 
 type RowKey = keyof typeof Keys;
@@ -34,69 +41,122 @@ function toNumber(input: string) {
 	return Number(input.trim().replace(/,/g, ''));
 }
 
-function toDate(input: string) {
-	return input;
+function getDayOfWeek(input: string) {
+	return moment(input, 'jYYYY/jMM/jDD').weekday();
 }
 
-class StockScrape {
-	browser!: Puppeteer.Browser;
+function getWeekOfYear(input: string) {
+	return moment(input, 'jYYYY/jMM/jDD').jWeek();
+}
 
-	async scrape(id: number, pageNumber = 1) {
+class SymbolScrape {
+	data: DateObject[];
+	dest: string;
+
+	constructor(private browser: Puppeteer.Browser, private symbol: Symbol) {
+		console.log(`Scraping: ${symbol.code}`);
+		this.data = [];
+		this.dest = path.resolve(__dirname, '..', 'output', `${this.symbol.code}.csv`);
+	}
+
+	public async start() {
+		await this.scrape();
+		return await this.save();
+	}
+
+	private async scrape(count = 200) {
 		const page = await this.browser.newPage();
-		await page.goto(`http://tsetmc.com/Loader.aspx?ParTree=151311&i=${id}`, {
+		console.log('Fetching main page');
+		await page.goto(`http://tsetmc.com/Loader.aspx?ParTree=151311&i=${this.symbol.id}`, {
 			waitUntil: 'domcontentloaded',
 		});
 		const selector = 'a.torquoise';
+		console.log('Fetching history');
 		await page.evaluate((selector) => document.querySelectorAll(selector)[0].click(), selector);
 		await new Promise((r) => setTimeout(r, 3000));
-		const $ = cheerio.load(await page.content());
-		const arr: CheerioElement[] = [];
-		$('.objbox .obj tr').each((_, i) => {
-			i.children
-				.filter((v) => v.name == 'td')
-				.forEach((v) =>
-					v.children.forEach((vi) => {
-						if (vi.type == 'text') {
-							arr.push(vi);
-						} else if (vi.name == 'span') {
-							arr.push(...vi.children);
-						}
-					})
-				);
-		});
-		const total: DateObject[] = [];
+		let current = 0;
+		let pageNumber = 0;
+		while (current < count) {
+			const selector2 = '.pagingBlock a';
+			await page.evaluate(
+				(selector, page) => document.querySelectorAll(selector)[page].click(),
+				selector2,
+				pageNumber++
+			);
+			const $ = cheerio.load(await page.content());
+			const arr: CheerioElement[] = [];
+			$('.objbox .obj tr').each((_, i) => {
+				i.children
+					.filter((v) => v.name == 'td')
+					.forEach((v) =>
+						v.children.forEach((vi) => {
+							if (vi.type == 'text') {
+								arr.push(vi);
+							} else if (vi.name == 'span') {
+								arr.push(...vi.children);
+							}
+						})
+					);
+			});
+			current = this.transform(arr);
+			console.log('Checking next page');
+		}
+		console.log(`Fetched ${this.data.length} days`);
+	}
+
+	private transform(input: CheerioElement[]) {
 		let obj: DateObject = {};
 		let i = 0;
-		for (const element of arr) {
+		for (const element of input) {
 			if (element.data!.trim().length == 0) {
-				console.log(obj);
-				total.push(obj);
+				if (obj.Volume) {
+					this.add(obj);
+				}
 				i = 0;
 				obj = {};
 			} else {
 				const enumKey = getEnumKeyByEnumValue(i++);
 				if (enumKey == 'PreviousDate') continue;
 				if (enumKey == 'DateJalaali') {
-					obj[enumKey] = toDate(element.data!);
+					obj[enumKey] = element.data!;
+					obj['DayOfWeek'] = getDayOfWeek(element.data!);
+					obj['WeekOfYear'] = getWeekOfYear(element.data!);
 				} else {
 					obj[enumKey] = toNumber(element.data!);
 				}
 			}
 		}
-		const selector2 = '.pagingBlock a';
-		await page.evaluate((selector) => document.querySelectorAll(selector)[1].click(), selector2);
-		await page.screenshot({ path: 'example.png' });
+		return this.data.length;
 	}
 
-	async start() {
-		this.browser = await Puppeteer.launch({ defaultViewport: { width: 1920, height: 1080 } });
-		for await (const symbol of symbols) {
-			await this.scrape(symbol.id);
-			await new Promise((r) => setTimeout(r, 5000));
+	private add(row: DateObject) {
+		const dupp = this.data.filter((v) => v.DateJalaali == row.DateJalaali);
+		if (dupp.length == 0) {
+			this.data.push(row);
 		}
-		await this.browser.close();
+	}
+
+	private async save() {
+		console.log('Saving');
+		const csvStream = csv.format({ headers: true });
+		csvStream.pipe(fs.createWriteStream(this.dest));
+		for (const row of this.data) {
+			csvStream.write(row);
+		}
+		csvStream.end();
+		console.log(`Saved to ${this.dest}`);
 	}
 }
 
-const scrapper = new StockScrape();
-scrapper.start();
+async function start() {
+	const browser = await Puppeteer.launch({ defaultViewport: { width: 1920, height: 1080 } });
+	console.log('Starting to scrape');
+	for await (const symbol of symbols) {
+		const symbolScrape = new SymbolScrape(browser, symbol);
+		await symbolScrape.start();
+		await new Promise((r) => setTimeout(r, 1000));
+	}
+	await browser.close();
+}
+
+start();
